9b9036694ec6f545c9046cf05e59130f664711e1
Tomi <tikawarder@gmail.com>
2023-03-16 11:39:50 +0100

    Create empty Spring project

---
+++ src/test/java/hu/tamasbiro/JobSearch/JobSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.tamasbiro.JobSearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
